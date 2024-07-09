52c74b00277f8cf89a10a0666280ddbae0e88f09
federico <pontonio.federico@gmail.com>
2024-07-09 10:59:39 +0200

    Adds receiveAttack method

--- tests/constructors.test.js
+++ tests/constructors.test.js
@@ -19,15 +19,26 @@ test('game board 2d array gets created', () => {
 });
 test('boat object gets placed in appropriate board slot', () => {
   let player1Board = Gameboard();let battlestarGalactica= Ship(7,'battlestarGalactica');
-  player1Board.placeBoat(battlestarGalactica, 0, 1);
-  expect(player1Board.board[0][1].boatInPlace.name).toBe("battlestarGalactica");
-  expect(player1Board.board[0][1].boatInPlace.size).toBe(7);
+  player1Board.placeBoat(battlestarGalactica, Gameboard().possiblePlacements(battlestarGalactica.size, 0, 0).right);
+  expect(player1Board.board[2][0].boatInPlace.name).toBe("battlestarGalactica");
+  expect(player1Board.board[1][0].boatInPlace.size).toBe(7);
 });
 //need to make sure that the size of the boat corrisponds to the number of occupied cells and that the cells are adiacent.
 test('excludes illegal placements', () => {
-  let player1Board = Gameboard();let battlestarGalactica= Ship(3,'battlestarGalactica');
-  expect(Gameboard().possiblePlacements(battlestarGalactica.size, 0, 0)).toMatchObject([[[0,0],[1,0],[2,0]], [[0,0],[0,1],[0,2]]]);
-  expect(Gameboard().possiblePlacements(battlestarGalactica.size, 9, 0)).toMatchObject([[[7,0],[8,0],[9,0]], [[9,0], [9,1],[9,2]]]);
-  expect(Gameboard().possiblePlacements(battlestarGalactica.size, 0, 3)).toMatchObject([[[0,3],[1,3],[2,3]], [[0,1],[0,2],[0,3]], [[0,3],[0,4],[0,5]]]);
-  expect(Gameboard().possiblePlacements(5, 4, 6)).toMatchObject([[[0,6],[1,6],[2,6],[3,6],[4,6]], [[4,6],[5,6],[6,6],[7,6],[8,6]], [[4,2],[4,3],[4,4],[4,5],[4,6]]]);
+  let battlestarGalactica= Ship(3,'battlestarGalactica');
+  expect(Gameboard().possiblePlacements(battlestarGalactica.size, 0, 0)).toMatchObject({"down": [[0, 0], [0, 1], [0, 2]], "letf": null, "right": [[0, 0], [1, 0], [2, 0]], "up": null});
+  expect(Gameboard().possiblePlacements(battlestarGalactica.size, 9, 0)).toMatchObject({"down": [[9, 0], [9, 1], [9, 2]], "letf": [[7, 0], [8, 0], [9, 0]], "right": null, "up": null});
+  expect(Gameboard().possiblePlacements(battlestarGalactica.size, 0, 3)).toMatchObject({"down": [[0, 3], [0, 4], [0, 5]], "letf": null, "right": [[0, 3], [1, 3], [2, 3]], "up": [[0, 1], [0, 2], [0, 3]]});
+  expect(Gameboard().possiblePlacements(5, 4, 6)).toMatchObject({"down": null, "letf": [[0, 6], [1, 6], [2, 6], [3, 6], [4, 6]], "right": [[4, 6], [5, 6], [6, 6], [7, 6], [8, 6]], "up": [[4, 2], [4, 3], [4, 4], [4, 5], [4, 6]]});
+});
+test('receive attack without hitting a boat', () => {
+  let player1Board = Gameboard();
+  expect(player1Board.board[4][5].hasBeenTargeted).toBeFalsy();//before the hit
+  expect(player1Board.receiveAttack(4, 5)).toBe('No ship has been hit.')
+  expect(player1Board.board[4][5].hasBeenTargeted).toBeTruthy();//after the hit
+});
+test('receive attack damageing a boat', () => {
+  let battlestarGalactica = Ship(4,'battlestarGalactica');let player1Board = Gameboard();
+  player1Board.placeBoat(battlestarGalactica, player1Board.possiblePlacements(battlestarGalactica.size, 4, 5).right);
+  expect(player1Board.receiveAttack(4, 5)).toBe(player1Board.board[4][5].boatInPlace.name+' has been hit!')
 });
